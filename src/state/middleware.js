bc9e3a6c39db01acd72976c3d3488e7cdf068da8
Raul Mabe <raulmateob@gmail.com>
2020-04-17 20:54:13 +0200

    dew

--- src/state/middleware.js
+++ src/state/middleware.js
@@ -17,7 +17,8 @@ export function fetchProjects() {
 
         const newProjects = projects.map((project) => {
           var aux = project;
-          aux.mocks = project.mabe.path_to_mock.map((mockPath) => {
+          console.log(aux);
+          aux.mocks = project.mabe.path_to_mocks.map((mockPath) => {
             return `https://cdn.jsdelivr.net/gh/Rahuvich/${project.name}@master${mockPath}`;
           });
 
