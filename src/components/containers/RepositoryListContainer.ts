8f50d2f71b1e9476b4eba550eac056cb0245cb63
Raul <raulmateob@gmail.com>
2020-11-03 15:41:15 +0100

    removing logs

--- src/components/containers/RepositoryListContainer.ts
+++ src/components/containers/RepositoryListContainer.ts
@@ -4,19 +4,14 @@ import RepositoryList from "../ui/RepositoryList";
 
 const mapStateToProps = (state: WebState) => {
   let tags: string[] = [];
-
-  console.log(state.projects);
-
   state.projects.forEach(
     (project: Project) => (tags = tags.concat(project.mabe.tag_tools))
   );
-
   tags = tags.filter((item, index) => {
     return tags.indexOf(item) === index;
   });
 
   let projects: Project[] = state.projects;
-
   if (state.filteringByTags) {
     projects = projects.filter((project) =>
       project.mabe.tag_tools.some((tag) => state.tagsSelected.includes(tag))
